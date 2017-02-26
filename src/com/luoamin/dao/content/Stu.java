9b1f4fbb1cdcfa31a4dfff228ed47a1a0d251445
luoamin <2293121112@qq.com>
2017-02-26 12:34:46 +0800

    测试修改的类

--- src/com/luoamin/dao/content/Stu.java
+++ src/com/luoamin/dao/content/Stu.java
@@ -3,4 +3,13 @@ package com.luoamin.dao.content;
 public class Stu {
 	/*test property*/
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
